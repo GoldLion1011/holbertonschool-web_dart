7f1514205ab09d31c75c5a43947a0156a06c5797
GoldLion1011 <yogirose79@gmail.com>
2023-10-13 21:35:51 +0000

    task 6 again since you want less from me

--- 0x00-hello-dart/6-positive_or_negative.dart
+++ 0x00-hello-dart/6-positive_or_negative.dart
@@ -1,22 +1,11 @@
 void main(List<String> args) {
-  if (args.length != 1) {
-    print("Please provide one argument.");
-    return;
-  }
+  var num = int.parse(args[0]);
 
-  String input = args[0];
-  int number = int.tryParse(input);
-
-  if (number == null) {
-    print("Invalid input. Please provide a valid integer.");
+  if (num > 0) {
+    print('${num} is positive');
+  } else if (num < 0) {
+    print('${num} is negative');
   } else {
-    print("$number");
-    if (number > 0) {
-      print("is positive");
-    } else if (number == 0) {
-      print("is zero");
-    } else {
-      print("is negative");
-    }
+    print('${num} is zero');
   }
 }
