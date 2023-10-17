7ca90edb02478e9ca106275edb32fe182b26ee3e
GoldLion1011 <yogirose79@gmail.com>
2023-10-17 17:59:03 +0000

    task 3

---
+++ 0x03-asynchronous/3-greet_user.dart
@@ -0,0 +1,29 @@
+import '3-util.dart';
+import 'dart:convert';
+
+Future<String> greetUser() async {
+  try {
+    final userData = await fetchUserData();
+    final Map<String, dynamic> userMap = json.decode(userData);
+    final String username = userMap['username'];
+    return 'Hello $username';
+  } catch (error) {
+    return 'error caught: $error';
+  }
+}
+
+Future<String> loginUser() async {
+  try {
+    final isUser = await checkCredentials();
+    print('There is a user: $isUser');
+    
+    if (isUser) {
+      final greeting = await greetUser();
+      return greeting;
+    } else {
+      return 'Wrong credentials';
+    }
+  } catch (error) {
+    return 'error caught: $error';
+  }
+}
